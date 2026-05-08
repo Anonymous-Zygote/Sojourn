7a86cee87baf17cd4083f7da70ec08ea61703faa
Anonymous-Zygote <zygoteanonymous@gmail.com>
2026-05-07 23:26:06 -0400

    Add docs/supabase-config.js

---
+++ docs/supabase-config.js
@@ -0,0 +1,5 @@
+window.ALIGNED_FOUNDATION_SUPABASE = {
+  url: "https://ecpavulnlyxonjfnnczn.supabase.co",
+  anonKey: "sb_publishable__A8qLZd_dg57cOJtlREgtw_pSQz-WgO",
+  tableName: "shared_results"
+};
